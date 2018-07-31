7a8f1e2fcea64491e626abbf4010b1947f1bda7b
tan-t <lee30x@gmail.com>
2018-07-31 21:05:15 +0900

    eslint --fix

--- src/api/contoller/ChatController.js
+++ src/api/contoller/ChatController.js
@@ -19,5 +19,3 @@ export class ChatController {
     next();
   }
 }
-
-
--- src/api/contoller/HistoryController.js
+++ src/api/contoller/HistoryController.js
@@ -5,10 +5,8 @@ export class HistoryController {
   }
 
   async list(ctx, next) {
-    const res = await History.paging(10,0);
+    const res = await History.paging(10, 0);
     ctx.body = res;
     next();
   }
 }
-
-
