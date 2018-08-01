20e39c1431554649fe7911fac28e83a069c851ca
tan-t <lee30x@gmail.com>
2018-08-01 22:20:42 +0900

    history/listのresponseが仕様通りでない部分を修正

--- src/api/contoller/HistoryController.js
+++ src/api/contoller/HistoryController.js
@@ -2,8 +2,16 @@ import History from '../model/History';
 
 export default class HistoryController {
   async list(ctx, next) {
-    const res = await History.paging(10, 0);
-    ctx.body = res;
+    const histories = await History.paging(10, 0);
+    ctx.body = histories.map(history=>(new HistoryResponse(history)));
     next();
+  }
+}
+
+class HistoryResponse {
+  constructor(model) {
+    this.user_input = model.user_input;
+    this.response_timestamp = model.response_timestamp;
+    this.bot_response = model.bot_response;
   }
 }
